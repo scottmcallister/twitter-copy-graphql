0b6e90e1b7de5b3dd26b10ef3c61c99b58c24097
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-02-18 13:32:22 -0400

    add bootstrap to project

--- apollo-ui/src/index.js
+++ apollo-ui/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
