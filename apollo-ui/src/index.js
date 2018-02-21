3f8d7e0381645274a87a37c0e76000b04ef78c5d
McAllister, Scott <smcallister@cvent.com>
2018-02-21 13:30:00 -0400

    store logged in user in client with apollo-link-state

--- apollo-ui/src/index.js
+++ apollo-ui/src/index.js
@@ -5,11 +5,37 @@ import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { withClientState } from 'apollo-link-state';
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link';
 
-// Pass your GraphQL endpoint to uri
-const client = new ApolloClient({ uri: 'http://localhost:8080/graphql' });
+const cache = new InMemoryCache();
+const httpLink = new HttpLink({
+    uri: 'http://127.0.0.1:8080/graphql'
+});
+
+const defaultState = {
+    loggedInUser: {
+        __typename: 'User',
+        name: "Donald J. Trump",
+        handle: "realDonaldTrump"
+    }
+};
+
+const stateLink = withClientState({
+    cache,
+    defaults: defaultState
+});
+
+const link = ApolloLink.from([stateLink, httpLink]);
+
+const client = new ApolloClient({ 
+    link: link,
+    cache
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
