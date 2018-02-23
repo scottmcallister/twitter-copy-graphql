0e3a6e71e703339441930fbbf08ffbd071e5029f
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-02-22 20:07:50 -0400

    set default client side state

--- apollo-ui/src/index.js
+++ apollo-ui/src/index.js
@@ -7,9 +7,17 @@ import './index.css';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { defaults, resolvers } from './resolvers';
 
 // Pass your GraphQL endpoint to uri
-const client = new ApolloClient({ uri: 'http://localhost:8080/graphql' });
+const client = new ApolloClient({ 
+    uri: 'http://localhost:8080/graphql',
+    connectToDevTools: true,
+    clientState: {
+        defaults,
+        resolvers
+    }
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
---
+++ apollo-ui/src/resolvers.js
@@ -0,0 +1,10 @@
+export const defaults = {
+    loggedInUser: {
+        id: 1,
+        name: 'Donald J. Trump',
+        handle: 'realDonaldTrump'
+    },
+    loggedIn: true
+};
+
+export const resolvers = {};
